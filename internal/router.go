55a584a128284b546a0411930cbbf688d264b943
Amirul Azreen <143872030+amirulazreen@users.noreply.github.com>
2024-03-16 23:49:53 +0800

    Update router.go

--- internal/router.go
+++ internal/router.go
@@ -178,11 +178,11 @@ func Addmany(db *gorm.DB) gin.HandlerFunc {
 
 func Random(i int) Product {
     return Product{
-        Item:     "Hammer " + strconv.Itoa(i),
-        Price:    float32(rand.Intn(10000)) / 100, 
+        Item:     "Hammer " + strconv.Itoa(i+1),
+        Price:    float32(rand.Intn(10000)+1) / 100, 
         Quantity: rand.Intn(100)+1,       
         Supplier: Supplier{
-            Name:    "Hammer Shop " + strconv.Itoa(i),
+            Name:    "Hammer Shop " + strconv.Itoa(i+1),
             Address: "23 Taman Bakti, Taman Perkasa Sakti, 2300 Kuala Lumpur",
             Tel:     "020 020 2020",
         },
