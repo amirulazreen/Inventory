a9ecc1b7a4abb5ca54c59ee5c8f680a4720cc73d
Amirul Azreen <143872030+amirulazreen@users.noreply.github.com>
2024-03-16 13:53:14 +0800

    Update database.go

--- internal/database.go
+++ internal/database.go
@@ -1,20 +1,30 @@
 package internal
 
 import (
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 type Product struct {
-	gorm.Model
-	Item     string   `json:"item"`
-	Price    float32  `json:"price"`
-	Quantity int      `json:"quantity"`
-	Supplier Supplier `gorm:"foreignKey:ProductID" json:"supplier"`
+    gorm.Model
+    Item     string   `json:"item" gorm:"not null;unique"`
+    Price    float32  `json:"price" gorm:"not null"`
+    Quantity int      `json:"quantity" gorm:"not null"`
+    Supplier Supplier `gorm:"foreignKey:ProductID" json:"supplier"`
 }
 
 type Supplier struct {
-	Name      string `json:"name"`
-	Address   string `json:"address"`
-	Tel       string `json:"tel"`
-	ProductID uint
+    Name      string `json:"name" gorm:"not null"`
+    Address   string `json:"address" gorm:"not null"`
+    Tel       string `json:"tel" gorm:"not null"`
+    ProductID uint
+}
+
+func createdb() {
+    db, err := gorm.Open(sqlite.Open("Inventory4.db"), &gorm.Config{})
+    if err != nil {
+        panic("failed to connect database")
+    }
+
+    db.AutoMigrate(&Product{}, &Supplier{})
 }
